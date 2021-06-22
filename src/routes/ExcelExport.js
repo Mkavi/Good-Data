47967797ae5ff75472a62f1a201bb06bbccd3d13
Harish Kommuri <harishbabukommuri@gmail.com>
2021-06-22 17:20:36 +0530

    changes for ts to js

--- src/routes/ExcelExport.js
+++ src/routes/ExcelExport.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { InsightView } from "@gooddata/sdk-ui-ext";
-import { ExampleWithExport} from "./ExampleWithExport.tsx";
+import { ExampleWithExport } from "./ExampleWithExport";
 import "../sai.css";
 import { newAbsoluteDateFilter } from "@gooddata/sdk-model";
 
-
-
-
 export const ExcelExport = () => {
-    return (
-        <ExampleWithExport >
-            {(onExportReady) => (
-                <div style={{ height: 0, width: 0 }}>
-                    <InsightView
-                        insight="abG4RMpBhZyp"
-                        onExportReady={onExportReady}
-                    />
-                </div>
-            )}
-        </ExampleWithExport>
-    );
+  return (
+    <ExampleWithExport>
+      {onExportReady => (
+        <div style={{ height: 0, width: 0 }}>
+          <InsightView insight="abG4RMpBhZyp" onExportReady={onExportReady} />
+        </div>
+      )}
+    </ExampleWithExport>
+  );
 };
 export default ExcelExport;
-
-
