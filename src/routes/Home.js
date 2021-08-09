9ec4d3e564b04eb4bdb32b5c302fa543f7a8822c
AmirYpoint <amir@ypointanalytics.com>
2021-08-09 19:29:36 +0530

    Added leaflet js , facing issues, needs review

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -16,6 +16,8 @@ import {
   newNegativeAttributeFilter
 } from "@gooddata/sdk-model";
 import { InsightView } from "@gooddata/sdk-ui-ext";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import Page from "../components/Page";
 import "../scss/custom.scss";
 import * as Ldm from "../md/full";
@@ -634,6 +636,23 @@ const Home = () => {
           ]}
         />
       </GDModal>
+      <div>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </Page>
   );
 };
